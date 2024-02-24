0d0f8e643a8557d2dba0be08997a6766157fcf0b
MHA Akhoundi <mha.akhoundi@gmail.com>
2024-02-24 11:17:09 +0330

    Spliting UI into Partials and

--- helpers.php
+++ helpers.php
@@ -10,3 +10,34 @@
 function basePath($path=''){
     return __DIR__.'/'.$path;
 }
+
+/**
+ * Load a view
+ * @param mixed $name
+ * @return void
+ */
+
+ function loadView($name){
+
+    $viewPath = basePath("views/{$name}.view.php");
+    if (file_exists($viewPath)){
+        require $viewPath;
+    }else{
+        echo "View {$name} not found!";
+    }
+ }
+
+ /**
+ * Load a partial
+ * @param mixed $name
+ * @return void
+ */
+
+ function loadPartial($name){
+    $partialPath = basePath("views/partials/{$name}.php");
+    if(file_exists($partialPath)){
+        require $partialPath;
+    }else{
+        echo "Partial {$name} not found!";
+    }
+ }
